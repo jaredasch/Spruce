bd4b522b7821bc3e47371609ba11a1ab4c94fc68
Stephanie Weirich <sweirich@seas.upenn.edu>
2021-11-16 14:29:00 -0500

    21fa

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,5 @@ module Lib
     ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc :: String
+someFunc = "Hello CIS 552"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,5 @@
+import Test.HUnit
+import Test.QuickCheck
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
