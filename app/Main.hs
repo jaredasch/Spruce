ae9892ed06f6b32287c2ccdebbcbdc014b827b3d
Jared Asch <jaredasch1@gmail.com>
2022-12-18 21:33:41 -0500

    rename bql -> spr

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
-import BQLEvaluator
+import BQLEvaluator (evalProgramFile)
 import BQLParser
+import GHC.IO.Handle (hFlush)
 import ParseLib
+import System.IO
 
 main :: IO ()
-main = putStrLn "Hello world"
+main = do
+  fname <- putStr "spruce> " *> hFlush stdout *> getLine
+  if fname == ":q"
+    then return ()
+    else do
+      result <- evalProgramFile fname
+      case result of
+        Left err -> putStrLn err
+        Right (Just returnVal) -> print returnVal
+        Right Nothing -> putStrLn "Success: no return value"
+      main
